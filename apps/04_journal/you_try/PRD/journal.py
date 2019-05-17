e15e887bec93f2674e95cdf1a268821b2695bcfb
pdeshpande <pdeshpande@nalumed.com>
2019-05-17 10:13:45 -0700

    journal app completed

--- apps/04_journal/you_try/PRD/journal.py
+++ apps/04_journal/you_try/PRD/journal.py
@@ -1,7 +1,15 @@
 import os
+"""
+This is Journal module
+"""
 
 
 def load(name):
+    '''
+    This method creates and loads a new journal
+    :param name: this base name of the journal to load
+    :return: a new journal data structure is populated with file data
+    '''
     data = []
     filename = get_full_path_name(name)
 
--- apps/04_journal/you_try/PRD/program.py
+++ apps/04_journal/you_try/PRD/program.py
@@ -4,11 +4,11 @@ import printHeader
 
 def run_event_loop():
     print('What do you want to do with your journal? ')
-    cmd = ''
+    cmd = 'EMPTY'
     journal_name = 'default'
     journal_data = journal.load(journal_name)       # list() # [] # alternative way
 
-    while cmd != 'x':
+    while cmd != 'x' and cmd:
         cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ')
         cmd = cmd.lower().strip()
 
@@ -16,7 +16,7 @@ def run_event_loop():
             list_entries(journal_data)
         elif cmd == 'a':
             add_entry(journal_data)
-        elif cmd != 'x':
+        elif cmd != 'x' and cmd:
             print("Sorry, we don't understand '{}'.".format(cmd))
 
     journal.save(journal_name, journal_data)
